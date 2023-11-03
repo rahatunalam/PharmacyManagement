70c1526277293a63fbb0a5a7332739231db68fc8
Rahat <Rahat@DESKTOP-24O7TMH>
2023-11-03 21:56:21 +0600

    First Commit

---
+++ src/dao/ConnectionProvider.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Rahat
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/pharmacy?useSSL=false","root","Rahat@#!15");
+            return con;
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+}
